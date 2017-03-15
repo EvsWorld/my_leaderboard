84204955b0ad64a446cdbb856451564bd0c13d3b
dromomaniac <evanhendrix1@Evans-MBP.lan>
2017-03-15 18:59:27 -0300

    Last changes before adding the user accounts

--- my_leaderboard.js
+++ my_leaderboard.js
@@ -1,11 +1,24 @@
 console.log("hello world (from console.log in .js file)");
 PlayersList = new Mongo.Collection('players');
 
+
+
 if(Meteor.isClient) {
+    // upsert used to rebuild the missing name field in Bill and Bob's
+    // documents
+    // PlayersList.upsert({ _id:"p74YGwgCukGaLAhwk" }, { $set: { name: // "Bill", score: [30] }});
+    // PlayersList.upsert({ _id:"YfTuMTJMaz85P4H" }, { $set: { name: // "Bob", score: [30] }});
+
+    // PlayersList.upsert({ score: "0" }, { $set: { score: [40] }});
+
+    // Used remove to remove an document
+    // PlayersList.remove({ _id: "fmYfTuMTJMaz85P4H" });
+
     console.log("Hello client");
+
     Template.leaderboard.helpers({
         'player': function(){
-            return PlayersList.find();
+            return PlayersList.find({}, { sort: {score: -1, name: 1} });
         },
         'numPlayers': function(){
             return PlayersList.find().count();
@@ -19,6 +32,10 @@ if(Meteor.isClient) {
         },
         'selectedPlayerIdFnct': function(){
             return (Session.get('selectedPlayerId'));
+        },
+        'selectedPlayer': function(){
+            var selectedPlayerId = Session.get('selectedPlayerId');
+            return PlayersList.findOne({ _id: selectedPlayerId });
         }
     });
 
@@ -35,6 +52,33 @@ if(Meteor.isClient) {
             // First arg of update method is to select which dictionary
             // in PlayersList collection to change. $inc is like -=
             PlayersList.update({ _id: Session.get('selectedPlayerId') }, { $inc: { score: -5 } });
+        },
+        // When the remove button is clicked on a chat message, delete
+        // that message. Doesn't work however.
+        'click .remove': function(){
+            PlayersList.remove(Session.get('selectedPlayerId'));
+        },
+    });
+
+
+    Template.addPlayerForm.events({
+        /** Event that triggers the execution of code whenever a user
+         * submits the form we just created. When an event is
+         * triggered from within a Meteor application, we’re able to
+         * access information about that event as it occurs. Whatever
+         * keyword is passed into the parentheses as the first
+         * argument of the event’s function becomes a reference for
+         * that event. This keeps refreshing the page when it is
+         * submitted so we have to put evt.preventDefault(); */
+        'submit form': function(evt){
+            evt.preventDefault();
+            var playerNameVar = event.target.playerName.value;
+            console.log(playerNameVar);
+            PlayersList.insert({
+                name: playerNameVar,
+                score: 0
+            });
+            evt.target.playerName.value = "";
         }
     });
 }
